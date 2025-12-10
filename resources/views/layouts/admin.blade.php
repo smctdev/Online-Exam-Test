d1c0b20188dc4d258b527586f7396ee0b95b5cdb
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-12-10 13:24:39 +0800

    yadf

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -22,7 +22,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css">
     <link rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="favicon.ico?v=2" type="image/x-icon">
     @stack('styles')
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
