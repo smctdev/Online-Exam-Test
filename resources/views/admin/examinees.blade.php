ee5ff6dcdfb46532b40db4d61a0e2b53c6f04471
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2025-12-10 17:11:52 +0800

    updated

--- resources/views/admin/examinees.blade.php
+++ resources/views/admin/examinees.blade.php
@@ -243,7 +243,7 @@
                     <h5 class="modal-title" id="createModalLabel">Add New Examinee</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <form method="POST" action="{{ route('store-examinee']) }}">
+                <form method="POST" action="{{ route('store-examinee') }}">
                     @csrf
                     <div class="modal-body">
                         <div class="row">
